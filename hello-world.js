71c4277e7b02c4ac34a64baa12d9675b5f101659
kolga <kolga@kolesa.kz>
2021-08-18 08:13:22 +0600

    Pushing my testing files

---
+++ hello-world.js
@@ -0,0 +1,14 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+
+
+ask(
+    "Вы согласны?",
+    function() { alert("Вы согласились."); },
+    function() { alert("Вы отменили выполнение."); }
+  );
+
+  
+console.log(getMinimalNumber(2, 1));
